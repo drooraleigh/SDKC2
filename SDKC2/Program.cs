b478938a95deb4cbc4b5fcc01f3a22950cce9b46
drooraleigh <drooraleigh@gmail.com>
2023-09-13 17:12:59 -0400

    first push

---
+++ SDKC2/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("How many records do you want to add? ");
+var numberOfRecords = int.Parse(Console.ReadLine());
+
+var recordList = new List<MyClass>();
+for (int i = 0; i < numberOfRecords; i++)
+{
+    // In this loop, populate the object's properties using Console.ReadLine()
+    var myClass = new MyClass();
+
+    Console.WriteLine("Enter the value for ");
+    myClass. = Console.ReadLine();
+
+
+    recordList.Add(myClass);
+}
+
+// Print out the list of records using Console.WriteLine()
